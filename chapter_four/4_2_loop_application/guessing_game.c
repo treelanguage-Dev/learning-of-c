92c646fd201992c35ba7f889ad935fe6d94923ba
treelanguage-Dev <treelanguage.dev@gmail.com>
2023-03-04 22:16:58 +0800

    add a game

--- chapter_four/4_2_loop_application/guessing_game.c
+++ chapter_four/4_2_loop_application/guessing_game.c
@@ -8,6 +8,20 @@
 
 int main(){
     srand(time(0));
-    int a=rand();
-    printf("%d",a);
+    int number =rand()%100+1;
+    int count =0;
+    int a=0;
+    printf("I've figured out an integer from 1 to 100\n");
+    //printf("%d",number);
+    do{
+        printf("Please guess my figure\n");
+        scanf("%d",&a);
+        count++;
+        if(a>number){
+            printf("Your guess is too big\n");
+        } else if(a<number){
+            printf("Your guess is too small\n");
+        }
+    } while (a!=number);
+    printf("Great! You guessed the number only %d times",count);
 }
