48224b750a318aac2c5f9aa3f42ca1d817c9f6bb
treelanguage-Dev <treelanguage.dev@gmail.com>
2023-03-05 17:26:47 +0800

    finish 5_3

--- chapter_five/5_3_circular_application/integer_decomposition.c
+++ chapter_five/5_3_circular_application/integer_decomposition.c
@@ -2,6 +2,27 @@
 // Created by treelanguage-Dev on 3/5/2023.
 //
 
-int main(){
+#include <stdio.h>
 
+int main() {
+    int x;
+    scanf("%d", &x);
+    x = 12345;
+    int t = 0;
+    do {
+        int d = x % 10;
+        t = t * 10 + d;
+        x /= 10;
+    } while (x > 0);
+    printf("x=%d, t=%d\n", x, t);
+    x = t;
+    do {
+        int d = x % 10;
+        printf("%d", d);
+        if (x > 9) {
+            printf("");
+        }
+        x /= 10;
+    } while (x > 0);
+    printf("\n");
 }
---
+++ chapter_five/5_3_circular_application/integer_decomposition_enhance.c
@@ -0,0 +1,30 @@
+//
+// Created by treelanguage-Dev on 3/5/2023.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int x;
+    //scanf("%d", &x);
+    x = 12345;
+    int t=x;
+    int mask = 1;
+    while (t > 9) {
+        t /= 10;
+        mask *= 10;
+    }
+    printf("x=%d, mask=%d\n",x, mask);
+    do {
+        int d = x / mask;
+        printf("%d", d);
+        if (mask > 9) {
+            printf(" ");
+        }
+        x %= mask;
+        mask /= 10;
+        //printf("x=%d, mask=%d, d=%d\n",x,mask,d);
+    } while (mask > 0);
+    printf("\n");
+}
