357691bc2b2c2c1d0f4989e5ac0e336d5f86dfd8
treelanguage-Dev <treelanguage.dev@gmail.com>
2023-03-05 15:39:17 +0800

    finished 5_1

---
+++ chapter_five/5_1_third_cycle/factorial.c
@@ -0,0 +1,22 @@
+//
+// Created by treelanguage-Dev on 3/5/2023.
+//
+
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int fact=1;
+    /*while(i<=n){
+        fact*=i;
+        i++;
+    }*/
+    /*for(int i=1;i<=n;i++){
+        fact*=i;
+    }*/
+    for(int i=n;i>1;i--){
+        fact*=i;
+    }
+    printf("%d!=%d\n",n,fact);
+}
---
+++ chapter_five/5_1_third_cycle/select_cycle.c
@@ -0,0 +1,24 @@
+//
+// Created by treelanguage-Dev on 3/5/2023.
+//
+
+#include <stdio.h>
+
+int main() {
+    int fact = 1, n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    //Equal to follow
+    int o = 1;
+    while (o <= n) {
+        fact *= o;
+        o++;
+    }
+    /**
+     * If there is a fixed number of times, use for
+     * If it must be executed once, use do_ while
+     * Use while for other i cases
+     */
+}
